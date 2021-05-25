416cad95d3690b3b4d687243382ae067103a1fbe
Nikki Grostefon <73565265+NikkiGrostefon@users.noreply.github.com>
2021-05-25 10:33:19 -0400

    challenge #4 complete

--- Challenges.js
+++ Challenges.js
@@ -146,3 +146,14 @@
 // }
 
 ///////////////////////////////////////////////////////////////////////////#4
+
+//1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement � (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+
+//2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+///////////////////////////////////////////////////////////////////////////#5
