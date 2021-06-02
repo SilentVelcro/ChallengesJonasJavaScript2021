ddc24f01406513b1478398903a4f877e1e4461da
Nikki Grostefon <73565265+NikkiGrostefon@users.noreply.github.com>
2021-06-02 09:01:16 -0400

    challenge  fundamentals 2

--- Challenges.js
+++ Challenges.js
@@ -253,3 +253,35 @@
 // }
 
 /////////////////////////////////////////////////////////////////////////// Part II - #4 - p.12
+
+// 1. Create an array 'bills' containing all 10 test bill values
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+const tips = []
+const totals = []
+
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+const calcTip = function (bill) {
+    return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+}
+
+// 4.
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        // sum = sum + arr[i];  
+        sum += arr[i];   // 280 & 281 do the same thing 
+    }
+    return sum / arr.length;
+}
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
